5fd39df60acaf0ce54cdaf486bd880b5beb9d18f
bryant <minh.pham.ag@gmail.com>
2020-11-22 15:14:14 +0700

    validator

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect(('mongodb://127.0.0.1:27017/task-manager-api'),{
     useNewUrlParser: true,
@@ -9,21 +10,34 @@ mongoose.connect(('mongodb://127.0.0.1:27017/task-manager-api'),{
 const User = mongoose.model('User',{
     name: {
         type: String,
-        require : true
+        require : true,
+        trim : true
     },
     age : {
         type: Number,
+        default: 0,
         validate(value){
             if(value<0) {
                 throw new Error('Age must be > 0')
+            }
+        }
+    }, email : {
+        type: String,
+        trim: true,
+        tolowercase: true ,
+        require: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw  new Error('Email is not valid')
             }
         }
     }
 })
 
 const testuser = new User({
-    name: 'Mike',
-    age: -1
+    name: '    Mike',
+    age: 15,
+    email: 'aAABC@abc.com'
 })
 
 testuser.save().then(()=>{
