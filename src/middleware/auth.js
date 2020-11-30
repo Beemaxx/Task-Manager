5035e6666b49f2824194638cd0ef42cacace68d2
bryant <minh.pham.ag@gmail.com>
2020-11-30 12:35:07 +0700

    token authentication

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,7 +6,7 @@ const User = require('../model/user')
 const auth = async (req,res,next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ','' )
-        const decoded = jwt.verify(token, 'sampletoken')
+        const decoded = jwt.verify(token, 'sampletoken') //*** Secret key ***
         const user = await User.findOne({ _id: decoded._id, 'tokens.token':token})
         console.log(user)
 
--- src/model/user.js
+++ src/model/user.js
@@ -50,7 +50,7 @@ const UserSchema = new mongoose.Schema( {
 
 UserSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({_id: user._id.toString() }, 'sampletoken')
+    const token = jwt.sign({_id: user._id.toString() }, 'sampletoken') //*** Secret key ***
 
     user.tokens = user.tokens.concat({token})
     await user.save()
